48a883bde9724b365255b07f888f76df33ce5482
AwFu17 <87723130+AwFu17@users.noreply.github.com>
2022-04-06 01:21:07 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
-
-const text1 = document.getElementById("ar1");
-const text2 = document.getElementById("ar2");
 const button = document.getElementById("btn");
-const fBtn = document.getElementById("fBtn");
-const sBtn = document.getElementById("sBtn");
+const $html = document.querySelector("html");
+let $x = 0;
+const copyArray = new Array(3);
+let isClick = false;
 
-let sHex = generateHexColor();
-let fHex = generateHexColor();
+var sHex = generateHexColor();
+var fHex = generateHexColor();
 document.body.style.background = toRGB(sHex, fHex);
-text1.innerHTML = `left hex: <span id="lHex">#${sHex.join('')}</span> `;
-text2.innerHTML = `right hex: <span id="rHex">#${fHex.join('')}</span>`;
 
 button.onclick = function(){
-    let sHex = generateHexColor();
-    let fHex = generateHexColor();
-    text1.innerHTML = `left hex: <span id="lHex">#${sHex.join('')}</span> `;
-    text2.innerHTML = `right hex: <span id="rHex">#${fHex.join('')}</span>`;
+    sHex = generateHexColor();
+    fHex = generateHexColor();
     console.log(toRGB(sHex, fHex));
     document.body.style.background = toRGB(sHex, fHex);
+    isClick = !isClick;
+    setTimeout(
+        ()=>{
+            isClick = !isClick;
+        }, 1
+    );
 }
-
-fBtn.onclick = function(){
-    const lHex = document.getElementById("lHex");
-    navigator.clipboard.writeText(lHex.textContent)
-    .then(() => {alert("copied")})
-    .catch(err => {console.log(err)})
-}
-
-sBtn.onclick = function(){
-    const rHex = document.getElementById("rHex");
-    navigator.clipboard.writeText(rHex.textContent)
-    .then(() => {alert("copied")})
-    .catch(err => {console.log(err)})
+ 
+$html.onclick = function(event){
+    if (!isClick){
+    console.log(event.offsetX);
+    $x = event.offsetX;
+    for (let i = 0; i < 3; i++){
+        console.log(average($html.offsetWidth, parseInt(sHex[i], 16), parseInt(fHex[i], 16),  parseInt($x)));
+        copyArray[i] = average($html.offsetWidth, parseInt(sHex[i], 16), parseInt(fHex[i], 16),  parseInt($x));
+    }
+    navigator.clipboard.writeText(toHexColor(copyArray))
+    .then(alert(toHexColor(copyArray) + " was copied!"))}
 }
 
 function generateHexColor(){
@@ -46,4 +45,16 @@ function toRGB(sArr, fArr){
     return `linear-gradient(to right,
         rgb(${parseInt(sArr[0], 16)}, ${parseInt(sArr[1], 16)}, ${parseInt(sArr[2], 16)}),
         rgb(${parseInt(fArr[0], 16)}, ${parseInt(fArr[1], 16)}, ${parseInt(fArr[2], 16)})`
+}
+
+function average(w, l, r, x){
+    return Math.round(((w-x)*l + x*r)/w);
+}
+
+function toHexColor(arr){
+    const rgb = new Array(3);
+    for (let i = 0; i < rgb.length;  i++){
+        rgb[i] = arr[i].toString(16);
+    }
+    return "#" + rgb.join('');
 }
